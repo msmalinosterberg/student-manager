c17e22336aa75e51f5cc052481d4bbd27dd370c2
Malin Österberg <msmalinosterberg@gmail.com>
2022-01-31 14:58:57 +0100

    lastname

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -10,7 +10,9 @@
           :to="'editStudent/' + index"
         >
           <v-list-tile-content>
-            <v-list-tile-title v-text="student.firstName"></v-list-tile-title>
+            <v-list-tile-title
+              v-text="student.firstName + ' ' + student.lastName"
+            ></v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
